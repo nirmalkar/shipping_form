a61731327e9ce796aea8a061a5bc5e8706169c6f
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-13 14:10:47 +0530

    INITIAL_COMMIT: start with eslint, scss bootstrap n all

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "bootstrap";
+
+import Shipping from "./pages/Shipping";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <Shipping />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/Shipping.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Shipping = () => {
+  return (
+    <div>
+      <h2>Shipping</h2>
+    </div>
+  );
+};
+export default Shipping;
